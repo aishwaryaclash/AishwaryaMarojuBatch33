b0d799dced24c426e57d31f3a0e5c2eea07ee147
aishwaryaclash <aishwaryamaroju5@gmail.com>
2024-01-11 09:28:08 -0600

    Student.java

--- batch33/src/Student.java
+++ batch33/src/Student.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private String name;
+    public Student(String name){
+        this.name=name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public static void main(String args[])
+    {
+        Student obj=new Student("Aishwarya ");
+        System.out.println("Student Name is :"+obj.getName());
+    }
+
 }
