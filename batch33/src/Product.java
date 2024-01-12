d3ef32804e782939394f1f6acb2f1b3e8ee93921
aishwaryaclash <aishwaryamaroju5@gmail.com>
2024-01-12 10:12:48 -0600

    src/.

--- batch33/src/Product.java
+++ batch33/src/Product.java
@@ -16,13 +16,13 @@ public class Product {
     public static void main(String args[])
     {
         Product q=new Product();
-        q.setPrice(50031);
+        q.setPrice(50031d);
         double r=q.getPrice();
         System.out.println("The price of Quantity of product q is :"+r);
         Product p=new Product();
-        p.setPrice(2400);
+        p.setPrice(2400d);
 
-        double priceOfProduct=p.getPrice(24.5);
+        double priceOfProduct=p.getPrice(24.5d);
         System.out.println("The price of the quantity of product p is :"+priceOfProduct);
 
     }
