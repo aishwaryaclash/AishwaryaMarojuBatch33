6deb2d40df31b2ad863cbcf8ce267de292435d52
aishwaryaclash <aishwaryamaroju5@gmail.com>
2024-01-25 11:10:07 -0600

    batch33/.

--- batch33/src/test/java/arraylists/RemoveLowGPAStudentTest.java
+++ batch33/src/test/java/arraylists/RemoveLowGPAStudentTest.java
@@ -1,5 +1,6 @@
 package arraylists;
 import arraylists.RemoveLowGPAStudent;
+import arraylists.Student;
 import org.testng.annotations.Test;
 import java.util.ArrayList;
 import static junit.framework.Assert.*;
@@ -23,11 +24,28 @@ public class RemoveLowGPAStudentTest {
      @Test
      void removeLowGpaStudent3()
      {
-         ArrayList<Student> studs=new ArrayList<>();
-         studs.add(new Student("Aishwarya","Maroju",3.66));
-         studs.add(new Student("Snehasri","Doma",3.2));
-         studs.add(new Student("Lahari","Chandra",3.0));
-         studs.add(new Student("Homakesh","Thokala",3.4));
+         ArrayList<Student> studs1=new ArrayList<>();
+
+         studs1.add(new Student("Aishwarya","Maroju",3.66));
+         studs1.add(new Student("Snehasri","Doma",3.2));
+         studs1.add(new Student("Lahari","Chandra",3.0));
+         studs1.add(new Student("Homakesh","Thokala",3.4));
+         ArrayList<Student> studs=studs1;
+         double totalGPA = 0;
+         for(Student s:studs)
+         {
+
+
+                 totalGPA += s.GPA;
+
+             }
+
+             double averageGPA = totalGPA / studs.size();
+
+         studs.removeIf(s -> s.GPA < averageGPA);
+
+         assertEquals(studs,RemoveLowGPAStudent.removeLowGPA(studs1));
+
 
 
      }
